5ff478c3647874f79622ff2bf9015a4d77879490
Leonardo <leosimao2010@gmail.com>
2023-05-02 22:01:17 -0300

    Search bar

--- components/Header.js
+++ components/Header.js
@@ -10,20 +10,23 @@ export default function Header() {
         <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
           <Image src="https://links.papareact.com/ocw" layou="fill" width={100} height={100} object-Fit='contain' />  
         </div>
-      </div> 
-        
-      <div className='relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer'>
-        <Image src="https://links.papareact.com/jjm" layou="fill" width={100} height={100} object-Fit='contain' />
-      </div>  
-      
-      <div>
-        <div>
-          <MagnifyingGlassIcon className='h-5 w-5'/>
+
+        <div className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer'>
+          <Image src="https://links.papareact.com/jjm" layou="fill" width={100} height={100} object-Fit='contain' />
+        </div>  
+
+        <div className='relative mt-1 p-3 rounded-md'>
+          <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+            <MagnifyingGlassIcon className='h-5 w-5 text-gray-500'/>
+          </div>
+
+          <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md'
+            type="text"
+            placeholder='Search'
+          />
         </div>
 
-        <input type="text" placeholder='Search'></input>
-      </div>
-
+      </div> 
     </div>
     
   )
